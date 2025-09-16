19a1fa82ba8faa712228333b07a1cfc23b03039d
Samu <serr73094@gmail.com>
2025-09-16 10:12:01 -0500

    deploy settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
@@ -10,12 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
     allowedHosts: ["sam-porfolio.up.railway.app"],
   },
-  plugins: [
-    react({
-      jsxImportSource: "react",
-    }),
-    mode === "development" && componentTagger()
-  ].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
